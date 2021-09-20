2b1fb5696c1762dbffa214094136431fab210e9d
Deyan Zubev <zubevdeyan@gmail.com>
2021-09-20 17:57:55 +0300

    first step in react NEtflix project

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      {/* Nav */}
+      <div className="nav false">
+        <img
+          className="nav__logo"
+          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+          alt=""
+        />
+      </div>
+      {/* End Nav */}
+      {/* Header */}
+      <header className="banner">
+        <div className="banner__contents">
+          <h1 className="banner__title">Ginny &amp; Georgia</h1>
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner__description">
+            Angsty and awkward fifteen year old Ginny Miller often feels more
+            mature than her thirty year old mother, the irresistible and dynamic
+            Georgia Miller...
+          </h1>
+        </div>
+        <div className="banner--fadeBottom"></div>
       </header>
+      {/* Header */}
     </div>
   );
 }
