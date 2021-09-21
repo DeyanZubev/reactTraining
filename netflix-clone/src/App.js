49f068599a17781a29b53606ffad15de4b0c4b5e
Deyan Zubev <zubevdeyan@gmail.com>
2021-09-21 18:30:55 +0300

    making components nav, header and rows also add firebase to the project with env config

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,37 +1,25 @@
+import Nav from "./components/nav/Nav";
+import Header from "./components/header/Header";
+import Row from "./components/row/Row";
+
 function App() {
   return (
     <div className="app">
       {/* Nav */}
-      <div className="nav false">
-        <img
-          className="nav__logo"
-          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
-          alt=""
-        />
-        <img
-          className="nav__avatar"
-          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
-          alt=""
-        />
-      </div>
+      <Nav />
       {/* End Nav */}
       {/* Header */}
-      <header className="banner">
-        <div className="banner__contents">
-          <h1 className="banner__title">Ginny &amp; Georgia</h1>
-          <div className="banner__buttons">
-            <button className="banner__button">Play</button>
-            <button className="banner__button">My List</button>
-          </div>
-          <h1 className="banner__description">
-            Angsty and awkward fifteen year old Ginny Miller often feels more
-            mature than her thirty year old mother, the irresistible and dynamic
-            Georgia Miller...
-          </h1>
-        </div>
-        <div className="banner--fadeBottom"></div>
-      </header>
+      <Header />
       {/* Header */}
+      {/* Row */}
+      <Row title='Netflix Originals' movieType='netflix-originals' />
+      <Row title='Trending Now' movieType='trending-now' />
+      <Row title='Top Rated' movieType='top-rated' />
+      <Row title='Action Movies' movieType='action-movies' />
+      <Row title='Horror Movies' movieType='horror-movies' />
+      <Row title='Romance Movies' movieType='romance-movies' />
+      <Row title='Documentaries Movies' movieType='documentaries-movies' />
+      {/* End Row */}
     </div>
   );
 }
